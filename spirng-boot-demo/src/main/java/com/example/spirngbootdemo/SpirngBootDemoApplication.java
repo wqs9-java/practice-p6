b35ed76fa35f427ddb0d3d3a0281beae4246c283
wqs9 <46802925+wqs9@users.noreply.github.com>
2020-10-06 00:08:15 +0800

    spring cloud
    spring cloud

---
+++ spirng-boot-demo/src/main/java/com/example/spirngbootdemo/SpirngBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spirngbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngBootDemoApplication.class, args);
+	}
+
+}
---
+++ spirng-boot-demo/src/test/java/com/example/spirngbootdemo/SpirngBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirngbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
